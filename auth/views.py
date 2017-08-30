3b0352bcae3c4ee149b3401f7c259d8ecc4a1fa3
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-29 21:23:23 -0700

    Add UserProfileViewSet

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from auth.serializers import UserProfileSerializer
+from auth.models import UserProfile
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and modifing user instances.
+    """
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    permission_classes = [permissions.AllowAny]
