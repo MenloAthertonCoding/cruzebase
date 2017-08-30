d70a7e19a8c8c9b972671bb8068b109c7fe34288
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-29 17:16:57 -0700

    Add UserProfile model and makemigrations

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Model that extends the django user model; adds extra metadata"""
+    # User one to one relationship
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # extra metadata
+    dob = models.DateField(verbose_name='Date of Birth')
+    car = models.BooleanField(verbose_name='Car?', default=False)
+    num_seats = models.PositiveSmallIntegerField(verbose_name='Number of Car seats', blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
