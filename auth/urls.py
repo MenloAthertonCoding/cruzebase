1d0d3337821e0980d67c81976bfac234b963589c
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-30 19:01:56 -0700

    Add url metadata to urlpatterns

--- auth/urls.py
+++ auth/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from auth import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserProfileViewSet)
+router.register(r'users', views.UserProfileViewSet, base_name='users')
 
 urlpatterns = router.urls
--- cruzebase/urls.py
+++ cruzebase/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('auth.urls')),
+    url(r'^api/', include('auth.urls', namespace='rest-auth')),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
