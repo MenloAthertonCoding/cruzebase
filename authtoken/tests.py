7388c761410883af0641344d33fd524119837d8e
Daniel Montgomery <d.montgrmry@gmail.com>
2017-09-10 18:59:57 -0700

    Refactor authtoken tests

--- authtoken/tests.py
+++ authtoken/tests.py
@@ -7,20 +7,13 @@ from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 from auth.models import UserProfile
 
 class AuthTokenTests(APITestCase):
-    __django_user = {
+    __django_user_dict = {
         'username': 'TEST',
         'password': 'password',
         'first_name': 'TEST',
         'last_name': 'TEST',
         'email': 'TEST@test.com'
     }
-
-
-    @classmethod
-    def _django_user_dict(cls):
-        # if not hasattr(cls, '__django_user'):
-        #     raise UnboundLocalError('__django_user used before assignment')
-        return cls.__django_user
 
     @classmethod
     def setUp(cls):
@@ -29,8 +22,8 @@ class AuthTokenTests(APITestCase):
         cls.client = APIClient()
 
         # Create a User and UserProfile object
-        user = DjangoUser(**cls._django_user_dict())
-        user.set_password(cls._django_user_dict()['password'])
+        user = DjangoUser(**cls.__django_user_dict)
+        user.set_password(cls.__django_user_dict['password'])
         user.save()
         cls.user_profile = UserProfile.objects.create(user=user, dob='1995-01-01')
 
@@ -44,9 +37,9 @@ class AuthTokenTests(APITestCase):
     #     assume the class member `_django_user`.
     #     """
     #     if not username:
-    #         username = self._django_user_dict()['username']
+    #         username = self.__django_user_dict['username']
     #     if not password:
-    #         password = self._django_user_dict()['password']
+    #         password = self.__django_user_dict['password']
 
     #     self.client.login(username=username, password=password)
 
@@ -55,11 +48,10 @@ class AuthTokenTests(APITestCase):
         Tests getting token results in HTTP 200 OK and response data
         has key `token`.
         """
-        user_data = self._django_user_dict()
         response = self.client.post(reverse('authtoken:obtain-auth-token'),
                                     {
-                                        'username': user_data['username'],
-                                        'password': user_data['password']
+                                        'username': self.__django_user_dict['username'],
+                                        'password': self.__django_user_dict['password']
                                     },
                                     format='json')
         self._assert_response_equal_status(response, status.HTTP_200_OK)
