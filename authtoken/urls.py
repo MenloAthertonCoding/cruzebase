78b6248c39cbad7b8000252b15d5b483b8ed0ce3
Daniel Montgomery <d.montgrmry@gmail.com>
2017-09-10 18:38:14 -0700

    Add authtoken namespace

--- authtoken/urls.py
+++ authtoken/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from authtoken import views
 
+app_name = 'authtoken'
+
 urlpatterns = [
     url(r'^$', views.ObtainAuthToken.as_view(), name='obtain-auth-token')
 ]
